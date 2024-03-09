d62750b000269fc6bafc7065314edb070e434190
scott <cvasiuc.dima@gmail.com>
2024-03-09 23:13:36 +0200

    Added the posibility to watch at the shortest path in browser and some bugs were fixed

--- main.py
+++ main.py
@@ -2,6 +2,12 @@ import os
 import random
 import math
 import osmnx as ox
+import time as t
+import folium
+import webbrowser as wb
+
+# Start the timer
+start = t.time()
 
 def generate_permutations(arr):
     result = []
@@ -16,7 +22,8 @@ def generate_permutations(arr):
             backtrack(start + 1)
             arr[start], arr[i] = arr[i], arr[start]
 
-    backtrack(0)
+    # Set the backtrack to 1 so that it excludes the 1st element
+    backtrack(1)
     return result
 
 file_path = os.path.join(os.path.dirname(__file__), "map.osm")
@@ -33,20 +40,51 @@ random_nodes = random.sample(nodes, 4)
 
 permutations = generate_permutations(random_nodes)
 
-shortest_path_legth = math.inf
+shortest_path_length = math.inf
+
+# A list to store the final route
+route_graph = []
 for permutation in permutations:
     path_length = 0
+    # A list to store the intermediate route
+    list_of_routes = []
     for i in range(len(permutation) - 1):
         # Getting the route from node X to node Y
         route = ox.shortest_path(G, permutation[i], permutation[i + 1], weight='length')
+        list_of_routes.append(route)
         # Convert the route to a GeoDataFrame
         route_gdf = ox.utils_graph.route_to_gdf(G, route, 'length')
         # Calculate the total length of the route
         path_length += route_gdf['length'].sum()
-    if path_length < shortest_path_legth:
-        shortest_path_legth = path_length
 
-print(f"{shortest_path_legth:.3f}")
+    if path_length < shortest_path_length:
+        shortest_path_length = path_length
+        # Save the route
+        route_graph = list_of_routes.copy()
 
-#TODO: 
-# To find a method how to display here the graph with the shortest path which will traverse every node given 
+start_node = G.nodes[route_graph[0][0]]
+route_map = folium.Map(location=[start_node['y'], start_node['x']], zoom_start=12)
+
+# Adding marker of the start point in our path
+start_marker = folium.Marker(location=(G.nodes[route_graph[0][0]]['y'], G.nodes[route_graph[0][0]]['x']), popup='Start', icon=folium.Icon(color='green')).add_to(route_map)
+
+# Iterate over paths and add a PolyLine for each path
+for path in route_graph:
+    # Create a list of (latitude, longitude) pairs for the path
+    path_coordinates = [(G.nodes[node]['y'], G.nodes[node]['x']) for node in path]
+
+    # Create a PolyLine for the path
+    folium.PolyLine(locations=path_coordinates, color='blue', weight=5, opacity=0.7).add_to(route_map)
+
+    # Adding destination markers on our path
+    folium.Marker(location=(G.nodes[path[-1]]['y'], G.nodes[path[-1]]['x']), popup='End', icon=folium.Icon(color='red')).add_to(route_map)
+
+
+# Save and show the route
+map_file_path="route_map.html"
+route_map.save(map_file_path)
+wb.open('file://' + os.path.realpath(map_file_path))
+
+# End the timer
+end = t.time()
+print(f"Completed in: {end - start:.2f} seconds.")
